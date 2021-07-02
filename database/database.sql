b2ec6a16239c3133255bcec3695988b7d99f47f2
Loryanar <47923247+Loryanar@users.noreply.github.com>
2021-07-01 22:59:56 -0400

    Add files via upload

---
+++ database/database.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE rotten;
+
+USE rotten;
+
+CREATE TABLE rottus (
+    id INT(20) NOT NULL  PRIMARY KEY,
+    nameus VARCHAR(20),
+    lastn VARCHAR(20),
+    email VARCHAR(30),
+    pass VARCHAR(80),
+   
+);
+
+
+DESCRIBE rottus;
